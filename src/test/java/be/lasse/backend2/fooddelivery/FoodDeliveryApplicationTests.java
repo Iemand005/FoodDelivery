ca34aa27e697d9e4c14534e008d3dc1d8036c3e4
Lasse <LauwerysLasse@gmail.com>
2024-11-26 19:01:32 +0100

    first commit

---
+++ src/test/java/be/lasse/backend2/fooddelivery/FoodDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package be.lasse.backend2.fooddelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodDeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
